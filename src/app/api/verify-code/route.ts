0346433a9d358af084c52a1ca26c6a3dbadb812a
vijaysinghkadela <132544545+vijaysinghkadela@users.noreply.github.com>
2024-11-04 19:50:48 +0530

    added OTP verification

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,72 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 500 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "Account verified Successfully",
+        },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification code has expired Please sign up again to get a new code",
+        },
+        { status: 200 }
+      );
+    }else{
+        return Response.json(
+            {
+              success: false,
+              message:
+                "Incorrect Verification Code",
+            },
+            { status: 200 }
+          );  
+    }
+
+
+
+
+
+
+
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      { status: 400 }
+    );
+  }
+}
